05ce7cc4cd18e00df5bb559f4900179da97946b8
Azizjan Ayupov <litpulla@mail.ru>
2021-08-20 09:38:52 +0600

    userSecrets

---
+++ KristaShop.WebUI/UserSecrets.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KristaShop.WebUI {
+    public class UserSecrets {
+        /*
+         * {
+  "ConnectionStrings": {
+    "KristaShopMysql": "server=localhost;user=root;password=root;database=krista_shop;TreatTinyAsBoolean=true;OldGuids=true"
+  },
+  "GlobalSettings": {
+    "FilesDirectoryPath": "C:\\Users\\User\\source\\repos\\KristaShop.WebUI\\wwwroot"
+  }
+}
+         */
+    }
+}
